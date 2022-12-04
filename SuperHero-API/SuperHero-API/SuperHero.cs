213206cf74fa75f58e448c61c36ba371837e7519
fsmiljkovski <56959771+fsmiljkovski@users.noreply.github.com>
2022-12-04 02:39:29 +0100

    Superhero App

---
+++ SuperHero-API/SuperHero-API/SuperHero.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperHero_API
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string firstName { get; set; } = String.Empty;
+        public string lastName { get; set; } = String.Empty;
+        public string Place { get; set; } = String.Empty;
+
+
+    }
+}
